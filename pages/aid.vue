559d4b3f0167638265cd5b8c157d84218f1e2217
Mithun Halder <mithun.tutorial.one@gmail.com>
2021-09-23 00:02:19 +0600

    Aid test page added

---
+++ pages/aid.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    aid page
+  </div>
+</template>
+
+<script>
+  export default {
+      beforeMount() {
+        const query = this.$route.query;
+        if(query.hasOwnProperty('aid') && query.aid != ''){
+          window.location = `${process.env.API_URL}/aid-url?aid=${query.aid}`
+        }else{
+          window.location = '/'
+        }
+      },
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
