c0bd6da74d2ef765e6844f734f3fe3ec9f96116d
Mithun Halder <mithun.tutorial.one@gmail.com>
2021-11-05 12:15:45 +0600

    Most visits to most viewed rename

--- components/search/FilterSearch.vue
+++ components/search/FilterSearch.vue
@@ -34,7 +34,7 @@
                   <strong class="dark">Most Favorited</strong>
                 </template> -->
                 <template v-if="sort_by == 'visits'">
-                  <strong class="dark">Most Visits</strong>
+                  <strong class="dark">Most Viewed</strong>
                 </template>
 
                  <svg class="svg-inline--fa fa-caret-down" data-v-6ea372ba="" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="caret-down" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path></svg>
@@ -58,7 +58,7 @@
                   >
                 </li> -->
                 <li>
-                  <a href="#" @click.prevent="sortBy('visits')">Most Visits</a>
+                  <a href="#" @click.prevent="sortBy('visits')">Most Viewed</a>
                 </li>
               </ul>
             </div>
