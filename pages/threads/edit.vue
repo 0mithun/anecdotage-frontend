ae1b16a3500d1f758f2268211f8480f7a0baf4cd
Mithun Halder <mithun.tutorial.one@gmail.com>
2021-11-24 21:10:39 +0600

    Submit enter added

--- pages/threads/edit.vue
+++ pages/threads/edit.vue
@@ -11,6 +11,7 @@
           action=""
           method="post"
           enctype="multipart/form-data"
+          @submit.prevent="updateAndGoThumb"
 
         >
           <div class="row">
@@ -420,9 +421,9 @@
                 </button>
                 <button
                   :disabled="form.busy"
-                  type="button"
+                  type="submit"
                   class="btn btn-primary"
-                  @click="updateAndGoThumb"
+
                 >
                   <span v-if="form.busy">
                     <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="spinner" class="svg-inline--fa fa-spinner" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M304 48c0 26.51-21.49 48-48 48s-48-21.49-48-48 21.49-48 48-48 48 21.49 48 48zm-48 368c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zm208-208c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48zM96 256c0-26.51-21.49-48-48-48S0 229.49 0 256s21.49 48 48 48 48-21.49 48-48zm12.922 99.078c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.491-48-48-48zm294.156 0c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48c0-26.509-21.49-48-48-48zM108.922 60.922c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.491-48-48-48z"></path>
